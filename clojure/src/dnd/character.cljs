373e71aa06d8253d8c61bf338f24bb13432285e6
Richard Kubina <richardkubina@eflexsystems.com>
2017-07-16 13:25:48 -0400

    move-character

--- clojure/src/dnd/character.cljs
+++ clojure/src/dnd/character.cljs
@@ -1,6 +1,14 @@
 (ns dnd.character)
 
-(def character {
-                :char "@"
-                :inventory []
-                })
+(def character {:char "@"
+                :inventory [] })
+
+(defn move-character [level character dx dy]
+  (let [get-params [level [dx dy]]
+        firstOne (apply get-in get-params)
+        updated-character (assoc character :x dx :y dy)
+        update-habs #(assoc firstOne :inhabitants [character])
+        updated-level (apply update-in (conj get-params update-habs))]
+    [updated-level updated-character]))
+
+
