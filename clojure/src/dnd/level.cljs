41b0528225c901e589098ec2830ac2f3aadf2132
Richard Kubina <richardkubina@eflexsystems.com>
2017-07-16 13:27:19 -0400

    level is a 2d array

--- clojure/src/dnd/level.cljs
+++ clojure/src/dnd/level.cljs
@@ -7,16 +7,23 @@
             :solid false
             })
 
-(def width  40)
-(def height 20)
+(def width  20)
+(def height 5)
 
 (defn make-block [x y]
   (let [solid (> (rand) 0.9)]
     (assoc block :x x :y y :solid solid)))
 
-(defn make-level []
-  (vec
-    (for [x (map inc (range width))
-          y (map inc (range height))]
-    (make-block x y))))
+(defn make-level
+  ([]
+   (make-level width height))
+
+  ([width height]
+   (let [one-indexed-range #(map inc (range %))
+         row-array (one-indexed-range width)
+         col-array (one-indexed-range height)
+         ]
+    (vec
+      (for [x row-array]
+        (vec (map #(make-block x %) col-array)))))))
 
