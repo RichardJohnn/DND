6ee80a90408b621727bb172c7a74f1ca4a848cfd
Richard Kubina <richardkubina@eflexsystems.com>
2017-08-17 22:43:04 -0400

    destructuring

--- clojure/src/dnd/character.cljs
+++ clojure/src/dnd/character.cljs
@@ -3,28 +3,25 @@
 (defonce character {:char "@"
                     :x 3
                     :y 2
-                    :inventory [] })
+                    :inventory []})
 
 (defn is-character [inhabitant] (= "@" (:char inhabitant)))
 
 (defn move-character! [level character dx dy]
   (let [{ox :x oy :y} @character
-        old-block (get-in @level [ox oy])
-        old-inhabitants (:inhabitants old-block)
+        {old-inhabitants :inhabitants} (get-in @level [ox oy])
         remove-character #(assoc % :inhabitants (remove is-character old-inhabitants))
         level-without-character (swap! level update-in [ox oy] remove-character)
         target-block (get-in level-without-character [dx dy])
         target-inhabitants (:inhabitants target-block)
         updated-character (swap! character assoc :x dx :y dy)
         update-habs #(assoc % :inhabitants (conj target-inhabitants updated-character))
-        updated-level (swap! level update-in [dx dy] update-habs)
-        ]
+        updated-level (swap! level update-in [dx dy] update-habs)]
     [level character]))
 
 (defn get-item! [level character]
   (let [{:keys [x y]} @character
-        block (get-in @level [x y])
-        old-inhabitants (:inhabitants block)
+        {old-inhabitants :inhabitants} (get-in @level [x y])
         gotten-inhabitant (first (remove is-character old-inhabitants))
 
         inventory (:inventory @character)
