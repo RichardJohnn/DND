d92fd57b1c37833ffcea44c8cb58a9af5130ec7a
Richard Kubina <richardkubina@eflexsystems.com>
2017-08-21 11:38:21 -0400

    can't walk through walls

--- clojure/src/dnd/character.cljs
+++ clojure/src/dnd/character.cljs
@@ -1,22 +1,24 @@
 (ns dnd.character)
 
 (defonce base-character {:char "@"
-                    :x 3
-                    :y 2
-                    :inventory []})
+                         :x 3
+                         :y 2
+                         :inventory []})
 
 (defn is-character [inhabitant] (= "@" (:char inhabitant)))
 
 (defn move-character! [level character dx dy]
-  (let [{ox :x oy :y} @character
-        {old-inhabitants :inhabitants} (get-in @level [ox oy])
-        remove-character #(assoc % :inhabitants (remove is-character old-inhabitants))
-        level-without-character (swap! level update-in [ox oy] remove-character)
-        target-block (get-in level-without-character [dx dy])
-        target-inhabitants (:inhabitants target-block)
-        updated-character (swap! character assoc :x dx :y dy)
-        update-habs #(assoc % :inhabitants (conj target-inhabitants updated-character))
-        updated-level (swap! level update-in [dx dy] update-habs)]
+  (let [target-block (get-in @level [dx dy])]
+    (if-let [target-block (not (:solid target-block))]
+      (let [{ox :x oy :y} @character
+            {old-inhabitants :inhabitants} (get-in @level [ox oy])
+            remove-character #(assoc % :inhabitants (remove is-character old-inhabitants))
+            level-without-character (swap! level update-in [ox oy] remove-character)
+            target-inhabitants (:inhabitants target-block)
+            updated-character (swap! character assoc :x dx :y dy)
+            update-habs #(assoc % :inhabitants (conj target-inhabitants updated-character))
+            updated-level (swap! level update-in [dx dy] update-habs)])
+      "can't walk there")
     [level character]))
 
 
