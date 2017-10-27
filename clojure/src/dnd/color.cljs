45371c615a6f51a6043c0240da9a44caecb2bf5e
Richard Kubina <richardkubina@eflexsystems.com>
2017-10-26 23:01:22 -0400

    name this color

--- clojure/src/dnd/color.cljs
+++ clojure/src/dnd/color.cljs
@@ -4,6 +4,22 @@
 (def COLOR
   (nodejs/require "color"))
 
+(def name-this-color
+  (nodejs/require "name-this-color"))
+
+(defn rgb-vec->str [rgb]
+  (apply str
+         (interleave
+           ["rgb(" ,,, "," ,,, "," ,,, ")"]
+           (conj rgb nil))))
+
+(defn color-name [color]
+  (-> color
+      rgb-vec->str
+      name-this-color
+      (aget 0)
+      .-title))
+
 (defn color-to-array [kolor]
   (-> kolor (.rgb) (.array)))
 
