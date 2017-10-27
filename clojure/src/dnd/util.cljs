3a853c70ecb2949b9e68775ee1a68c2ad04a6fae
Richard Kubina <richardkubina@eflexsystems.com>
2017-10-26 23:11:08 -0400

    maybe use this for better fov code

---
+++ clojure/src/dnd/util.cljs
@@ -0,0 +1,11 @@
+(ns dnd.util)
+
+(defn index-of
+  "return the index of the supplied item, or nil"
+  [v item]
+  (let [len (count v)]
+    (loop [i 0]
+      (cond
+        (<= len i)         nil,
+        (= item (get v i)) i,
+        :else              (recur (inc i ))))))
